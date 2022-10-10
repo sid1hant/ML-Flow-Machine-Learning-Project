5e3f57cfda4780a41584c0656e291f239c6fc994
Siddhant Singh <71932903+sid1hant@users.noreply.github.com>
2022-10-10 16:17:38 +0530

    Add files via upload

---
+++ run.py
@@ -0,0 +1,11 @@
+import os
+import numpy as np
+
+
+alphas=np.linspace(0.1,1.0,5)
+l1_ratios=np.linspace(0.1,1.0,5)
+
+for alpha in alphas:
+    for l1_ratio in l1_ratios:
+        print(f"logging experiment for alpha : {alpha}, l1_ratio: {l1_ratio}")
+        os.system(f"python demo.py -a  {alpha} -l1 {l1_ratio}")
